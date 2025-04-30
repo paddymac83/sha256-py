637d891c5111a63ec1a99a5b3d9a856781bbed68
paddymac <mcnabb_patrick@yahoo.com>
2025-04-30 21:38:31 +0100

    Added block class

--- block.py
+++ block.py
@@ -1,6 +1,18 @@
+import hashlib
+
 class Block():
     def __init__(self, data):
         self.data = data
         self.nonce = 0
-        self.hash = None
+        self.hash = hashlib.sha256()
+
+    def mine(self, difficulty):
+        self.hash.update(str(self).encode("utf-8"))
+        while int(self.hash.hexdigest(), 16) > 2**(256 - difficulty):
+            self.nonce += 1
+            self.hash = hashlib.sha256()
+            self.hash.update(str(self).encode("utf-8"))
+    
+    def __str__(self):
+        return "{}{}".format(self.data, self.nonce)
 
