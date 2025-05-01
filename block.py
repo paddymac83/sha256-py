2d857148319050632866a01e5080117cd0bb4aa3
paddymac <mcnabb_patrick@yahoo.com>
2025-05-01 22:33:26 +0100

    Added linked list in Chain class

--- block.py
+++ block.py
@@ -1,8 +1,9 @@
 import hashlib
 
 class Block():
-    def __init__(self, data):
+    def __init__(self, data, previous_block):
         self.data = data
+        self.previous_hash = previous_block.hash
         self.nonce = 0
         self.hash = hashlib.sha256()
 
@@ -14,5 +15,5 @@ class Block():
             self.hash.update(str(self).encode("utf-8"))
     
     def __str__(self):
-        return "{}{}".format(self.data, self.nonce)
+        return "{}{}{}".format(self.previous_block, self.data, self.nonce)
 
--- chain.py
+++ chain.py
@@ -1,2 +1,19 @@
+import hashlib
+
 class Chain():
-    pass
+    def __init__(self, difficulty):
+        self.difficulty = difficulty
+        self.blocks = []
+        self.pool = []
+
+    def proof_of_work(self, block):
+        hash = hashlib.sha256()
+        hash.update(str(block).encode("utf-8"))
+
+        return (block.hash.hexdigest() == hash.hexdigest() and 
+        int(hash.hexdigest(), 16) < 2**(256 - self.difficulty) and
+        block.previous_hash == block[-1].hash)
+    
+    def add_to_chain(self, block):
+        if self.proof_of_work(block):
+            self.blocks.append(block)
